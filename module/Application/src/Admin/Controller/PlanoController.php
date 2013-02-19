dd7a988d85c11e018dbd5ddc0c792d4eab0c62b8
André <andrework@gmail.com>
2013-02-19 19:53:17 -0300

    Nova atualização

--- module/Application/src/Admin/Controller/PlanoController.php
+++ module/Application/src/Admin/Controller/PlanoController.php
@@ -3,89 +3,89 @@
 namespace Admin\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController,
- 	Zend\View\Renderer\PhpRenderer,
-    Zend\View\Model\ViewModel;
+	Zend\View\Renderer\PhpRenderer,
+	Zend\View\Model\ViewModel;
 
 class PlanoController extends AbstractActionController {
-    
-    /**
-     *
-     * @var EntityManager
-     */
-    protected $em;
 	
-	    
-        /*
-     * @return EntityManager
-     */
+	/**
+	 *
+	 * @var EntityManager
+	 */
+	protected $em;
+	
+		
+		/*
+	 * @return EntityManager
+	 */
 
-    protected function getEm() {
-        if (null === $this->em)
-            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+	protected function getEm() {
+		if (null === $this->em)
+			$this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
 
-        return $this->em;
-    }
+		return $this->em;
+	}
 
-    public function indexAction()
-    {
-    	
-        $form = $this->getServiceLocator()->get("service_plano_form");
-        $request = $this->getRequest();
-        
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-            
-            if ($form->isValid()) {
-            	
-                $service = $this->getServiceLocator()->get("service_plano");
-                $records = $request->getPost()->toArray();
-                //$records['token'] = md5(uniqid(time()));
-                //var_dump($records);
-                $service->insert($records);
+	public function indexAction()
+	{
+		
+		$form = $this->getServiceLocator()->get("service_plano_form");
+		$request = $this->getRequest();
+		
+		if ($request->isPost()) {
+			$form->setData($request->getPost());
+			
+			if ($form->isValid()) {
 				
-				$msg['ref'] 	= "servico";
+				$service = $this->getServiceLocator()->get("service_plano");
+				$records = $request->getPost()->toArray();
+				//$records['token'] = md5(uniqid(time()));
+				//var_dump($records);
+				$service->insert($records);
+				
+				$msg['ref'] 	= "plano";
 				$msg['tipo']	= "success";	
 				$msg['cod_msg'] = "1";
-				$form->setData(array('nome'=>''));
+				$form->setData(array('nome'=>'','descricao'=>'','preco'=>'','dia_publicacao'=>''));
 				
-                //$service->SendEmail($records);    
+				//$service->SendEmail($records);    
 				//return $this->redirect()->toRoute('home-message',array('tipo'=>'fsuccess','ref'=>'register','cod_msg'=>'1'));
-            }
-        }
+			}
+		}
 		
 		$repository = $this->getEm()->getRepository("Application\Entity\Plano");
 		$obj_records = $repository->fetchPairs();
 		
 		//var_dump($obj_records->getArrayCopy());
 		return new ViewModel(array('form' => $form,'msg' => $msg,'dados'=>$obj_records));     
-    }
+	}
 
 	public function editAction()
 	{
 		
-        $form = $this->getServiceLocator()->get("service_servico_form");
-        $request = $this->getRequest();
-        
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-            
-            if ($form->isValid()) {
-            	
-                $service = $this->getServiceLocator()->get("service_servico");
-                $records = $request->getPost()->toArray();
-                //$records['token'] = md5(uniqid(time()));
-                //var_dump($records);
-                $service->update($records);
+		$form = $this->getServiceLocator()->get("service_servico_form");
+		$request = $this->getRequest();
+		
+		if ($request->isPost()) {
+			$form->setData($request->getPost());
+			
+			if ($form->isValid()) {
+				
+				$service = $this->getServiceLocator()->get("service_servico");
+				$records = $request->getPost()->toArray();
+				//$records['token'] = md5(uniqid(time()));
+				//var_dump($records);
+				$service->update($records);
 				
 				$msg['ref'] 	= "servico";
 				$msg['tipo']	= "success";	
 				$msg['cod_msg'] = "2";
 				//$form->setData(array('nome'=>''));
 				
-                //$service->SendEmail($records);    
+				//$service->SendEmail($records);    
 				//return $this->redirect()->toRoute('home-message',array('tipo'=>'fsuccess','ref'=>'register','cod_msg'=>'1'));
-            }
-        }
+			}
+		}
 		$repository = $this->getEm()->getRepository("Application\Entity\Servico");
 		
 		$id = $this->params()->fromRoute('id', 0);
@@ -115,7 +115,7 @@ class PlanoController extends AbstractActionController {
 		return $this->redirect()->toRoute('admin/servico');
 		
 	}
-    
+	
 
 
 
