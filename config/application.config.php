8fdc320c7f82d3d6cbb0e5024ce2dc35a03570a3
Andre Augusto <andrework@gmail.com>
2013-04-11 13:50:45 -0300

    all

---
+++ config/application.config.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+    'modules' => array(
+        'Application',
+        //'Album',
+        //'Livraria',
+        //'Login',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        //'ZfcBase',
+        //'ZfcUser',
+        //'ScnSocialAuth'
+        //'Livraria',
+        
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+);
