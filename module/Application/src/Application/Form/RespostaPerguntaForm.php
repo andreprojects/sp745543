27a8fecf3f20430a9425f7a3eb0ebc07dc4c279c
André <andrework@gmail.com>
2013-04-16 20:23:14 -0300

    test

---
+++ module/Application/src/Application/Form/RespostaPerguntaForm.php
@@ -0,0 +1,33 @@
+<?php
+namespace Application\Form;
+
+
+use Zend\Form\Form;
+use Application\Form\RespostaPerguntaFilter;
+
+
+class RespostaPerguntaForm extends Form
+{
+    public function __construct($baseUrl = null)
+    {
+        // we want to ignore the name passed
+        parent::__construct('respostapergunta');
+
+        $this->setAttribute('method', 'post');
+        $this->setInputFilter(new RespostaPerguntaFilter);
+        
+        $this->add(array(
+            'name' => 'msg_resposta',
+            'attributes' => array(
+                'type'  => 'Zend\Form\Element\Textarea',
+                'rows'  => 5,
+                'class' => 'span5',
+                'placeholder' => 'Digite sua Resposta'
+            ),
+            'options' => array(
+                'label' => 'Mensagem',
+            ),
+        ));
+
+    }
+}
